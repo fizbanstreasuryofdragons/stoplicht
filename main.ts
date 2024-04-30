f2a7f92c8ac268c1b7cdd1d6a8d4b9a08b5336c9
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-04-30 14:14:38 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -12,11 +12,11 @@ input.onButtonPressed(Button.B, function () {
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
         `)
     basic.pause(2000)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
